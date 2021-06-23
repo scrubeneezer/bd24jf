a6d2798bce5aa56c42a22bd3d6ea270d7e1e4b17
scrubeneezer <scrubeneezer [at] gmail.com>
2021-06-22 23:04:50 -0400

    scrub | site body and depl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,34 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'bd24jf2';
+
+  asteri = [
+    'Black Mage',
+    'White Mage',
+    'Vanguard',
+    'Monk',
+    'Thief',
+    'Bard',
+    'Beastmaster',
+    'Gambler',
+    'Berserker',
+    'Pictomancer',
+    'Ranger',
+    'Red Mage',
+    'Shieldmaster',
+    'Salve-Maker',
+    'Dragoon',
+    'Swordmaster',
+    'Spiritmaster',
+    'Oracle',
+    'Bastion',
+    'Phantom',
+    'Arcanist',
+    'Hellblade',
+    'Bravebearer',
+  ];
 }
